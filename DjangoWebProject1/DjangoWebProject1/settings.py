31588f571cb043e97f9104eab3af42daf4b93ff2
sunil <kushwahasunikl145@gmail.com>
2021-09-23 17:50:23 +0530

    crrect master page

--- DjangoWebProject1/DjangoWebProject1/settings.py
+++ DjangoWebProject1/DjangoWebProject1/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'DjangoWebProject1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
